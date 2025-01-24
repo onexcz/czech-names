6878c193e707e658d7403989ef9a8d3fd6fb7a19
Ondřej Exner <ondrej.exner@apptime.cz>
2025-01-24 14:43:45 +0100

    fix: csv plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dsv from '@rollup/plugin-dsv';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,20 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    dsv({
+      processRow: (row, id) => {
+          const keys = Object.keys(row);
+          if (keys.length === 2 && !isNaN(Number(row[keys[0]]))) {
+              // Check if the row structure is like "number,string" and the first element is a number
+              return {
+                  count: Number(row[keys[0]]), // Convert the first element to a number
+                  name: row[keys[1]], // Keep the second element as a string
+              };
+          } else {
+              return row;
+          }
+      },
+  }),
   ],
   resolve: {
     alias: {
